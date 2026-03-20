1e826f3e221c665619e989811abbb894ed5be4db
Maria Thurkadayil <mariapault2000@gmail.com>
2026-03-20 09:53:58 +0530

    Delete Assignment3 directory

--- Assignment3/test.py
+++
@@ -1,7 +0,0 @@
-import pytest
-import joblib
-from score import score
-def test_score():
-    model = joblib.load("model.joblib")
-    pred, prop = score("test", model, 0.5)
-    assert isinstance(pred, bool)
